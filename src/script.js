28505242d0b7db7949a03bde9e432713d8fea9a3
Colin Williams <williamsca20@gmail.com>
2022-01-14 20:08:55 -0500

    🤩 Added stars, made baritone rotate, dynamic screen size

--- src/script.js
+++ src/script.js
@@ -11,10 +11,21 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.z = 5
 
 // Add Renderer
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({
+	// canvas: document.getElementById('background'),
+	canvas: document.querySelector('#background'),
+})
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.setPixelRatio(window.devicePixelRatio)
-document.body.appendChild(renderer.domElement)
+
+// Set Window Size
+const setWindowSize = () => {
+	renderer.setSize(window.innerWidth, window.innerHeight)
+	camera.aspect = window.innerWidth / window.innerHeight
+	camera.updateProjectionMatrix()
+}
+setWindowSize()
+window.onresize = setWindowSize
 
 // Add Lighting
 hemiLight = new THREE.HemisphereLight(0xffeeb1, 0x080820, 4)
@@ -25,12 +36,14 @@ const modelLoader = new THREE.GLTFLoader()
 
 // Add Baritone Model
 // NOTE: Use glTF VS Code extension to convert .gltf to .glb
+let baritone
 modelLoader.load(
 	'assets/baritone.glb',
 	(gltf) => {
 		const scaleMultiplier = 0.05
 		gltf.scene.scale.set(scaleMultiplier, scaleMultiplier, scaleMultiplier)
-		scene.add(gltf.scene)
+		baritone = gltf.scene
+		scene.add(baritone)
 	},
 	undefined,
 	(error) => {
@@ -38,14 +51,30 @@ modelLoader.load(
 	}
 )
 
-// Add Orbit Controls
-const controls = new THREE.OrbitControls(camera, renderer.domElement)
-controls.update()
+// Add Stars
+const starGeometry = new THREE.SphereGeometry(0.2, 4, 4)
+const starMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff })
+
+const addStar = () => {
+	const sphere = new THREE.Mesh(starGeometry, starMaterial)
+	const [x, y, z] = Array(3)
+		.fill()
+		.map(() => THREE.MathUtils.randFloatSpread(200))
+	sphere.position.set(x, y, z)
+	scene.add(sphere)
+}
+
+for (let i = 0; i < 1000; i++) {
+	addStar()
+}
 
 // Animation Loop
 function animate() {
 	requestAnimationFrame(animate)
+
+	baritone.rotation.x += 0.01
+	baritone.rotation.y += 0.01
+
 	renderer.render(scene, camera)
-	controls.update()
 }
 animate()
