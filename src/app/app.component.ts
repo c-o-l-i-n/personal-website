b17a9a20433851ebc7601122d3062d112cbd0c27
Colin Williams <colin@colinaw.com>
2023-11-20 11:18:33 -0500

    📄 Add resume

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Meta } from '@angular/platform-browser';
 import { expandImageUrl } from './shared/util';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faFilePdf } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'colin-root',
@@ -10,6 +11,7 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 export class AppComponent implements OnInit {
   protected readonly faGithub = faGithub;
   protected readonly faLinkedin = faLinkedin;
+  protected readonly faFilePdf = faFilePdf;
 
   constructor(private meta: Meta) {}
 
