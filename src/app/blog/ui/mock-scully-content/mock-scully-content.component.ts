549e78a9e14b688f8178594e57cc15509412b4e2
Colin Williams <colin@colinaw.com>
2024-03-10 21:24:36 -0400

    🃏 Edit mock blog content page

--- src/app/blog/ui/mock-scully-content/mock-scully-content.component.ts
+++ src/app/blog/ui/mock-scully-content/mock-scully-content.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, inject } from '@angular/core';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'scully-content',
   templateUrl: './mock-scully-content.component.html',
 })
-export class MockScullyContentComponent {}
+export class MockScullyContentComponent implements OnInit {
+  private readonly el = inject(ElementRef);
+
+  ngOnInit() {
+    this.removeHostElement();
+  }
+
+  private removeHostElement(): void {
+    const hostElement = this.el.nativeElement;
+    const parentElement = hostElement.parentElement!;
+
+    // move all children out of the element
+    while (hostElement.firstChild) {
+      parentElement.insertBefore(hostElement.firstChild, hostElement);
+    }
+
+    // remove the empty element(the host)
+    parentElement.removeChild(hostElement);
+  }
+}
