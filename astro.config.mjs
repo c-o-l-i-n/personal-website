89cd129d6e356ef7981d78b1f731571b90c73363
Colin Williams <40863449+c-o-l-i-n@users.noreply.github.com>
2026-04-18 23:48:56 -0400

    🗺️ Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,22 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   markdown: {
     shikiConfig: {
       theme: 'one-dark-pro',
       wrap: false,
     },
   },
+
+  site: 'https://colinaw.com',
+  integrations: [sitemap()],
+
+  trailingSlash: 'never',
 });
